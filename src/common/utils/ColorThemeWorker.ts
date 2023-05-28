9df671cedfb4965ffff2b6886d052a63479400d9
Locked15 <almgal353@gmail.com>
2023-05-28 22:09:40 +0500

    Color theme related functionality moved to standalone file.

---
+++ src/common/utils/ColorThemeWorker.ts
@@ -0,0 +1,20 @@
+import ColorsTheme from '@/models/common/themes/ColorsTheme';
+
+import { CURRENT_THEME_KEY } from '../keys';
+
+export function getCurrentColorTheme(): ColorsTheme | null {
+  const localStorageValue = localStorage.getItem(CURRENT_THEME_KEY);
+  if (localStorageValue !== null) {
+    return JSON.parse(localStorageValue);
+  }
+  return null;
+}
+
+export function getDarkStateOfCurrentTheme(): boolean {
+  const localStorageValue = localStorage.getItem(CURRENT_THEME_KEY);
+  if (localStorageValue !== null) {
+    const theme: ColorsTheme = JSON.parse(localStorageValue);
+    return theme.isDarkTheme;
+  }
+  return false;
+}
