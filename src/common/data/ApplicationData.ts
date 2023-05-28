5f7f2c1604cef41d59f7cae957575d82d09ef08a
Locked15 <almgal353@gmail.com>
2023-05-28 21:38:42 +0500

    Application data was updated. Added new property with current saved teacher list.

--- src/common/data/ApplicationData.ts
+++ src/common/data/ApplicationData.ts
@@ -1,3 +1,7 @@
+import Teacher from '@/models/api/entities/v2/base/Teacher';
+
 export default class ApplicationData {
   public static availableGroups = new Array<string>();
+
+  public static availableTeachers = new Array<Teacher>();
 }
