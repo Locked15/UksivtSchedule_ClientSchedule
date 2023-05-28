a07f72c4e895ae0dab4cec1c97948067d5062938
Locked15 <almgal353@gmail.com>
2023-05-28 22:46:57 +0500

    Created basic entity for modern lesson classes.

---
+++ src/models/api/entities/v2/common/ModernAPILessonEntitiesParent.ts
@@ -0,0 +1,5 @@
+import ModernAPIEntitiesParent from './ModernAPIEntitiesParent';
+
+export default abstract class ModernAPILessonEntitiesParent extends ModernAPIEntitiesParent {
+  _isModernLesson: boolean | undefined;
+}
