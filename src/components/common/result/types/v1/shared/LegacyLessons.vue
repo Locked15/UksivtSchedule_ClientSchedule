3e4a5ebf65214808d99e4e9c4cda32881770ecea
Locked15 <almgal353@gmail.com>
2023-05-29 02:46:13 +0500

    LegacyLesson was renamed to LegacyLessons. This caused by purpose change.

---
+++ src/components/common/result/types/v1/shared/LegacyLessons.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Номер</th>
+        <th class="text-left">Название</th>
+        <th class="text-left">Преподаватель</th>
+        <th class="text-left">Аудитория</th>
+        <th class="text-left" title="На данный момент нереализовано..."><i>Время</i></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="lesson in lessons" :key="`${resultItemId}-${lesson.number}`" :class="lesson.changed ? 'altered-element' : ''">
+        <td>{{ lesson.number }}</td>
+        <td>{{ lesson.name }}</td>
+        <td>{{ lesson.teacher || 'Отсутствует' }}</td>
+        <td>{{ lesson.place || 'Отсутствует' }}</td>
+        <td>🕛 ... 🕧</td>
+      </tr>
+
+      <tr v-if="lessons.length < 1">
+        <td class="table-warning-text" colspan="2">Пары отсутствуют...</td>
+        <td class="table-warning-text">... или ...</td>
+        <td class="table-warning-text" colspan="2">... данные недоступны.</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script lang="ts">
+  import Lesson from '@/models/api/entities/v1/base/Lesson';
+  import { Options, Vue } from 'vue-class-component';
+
+  @Options({
+    props: {
+      resultItemId: Number,
+      lessons: Array<Lesson>,
+    },
+  })
+  export default class LegacyLessons extends Vue {
+    public resultItemId!: number;
+
+    public lessons!: Array<Lesson>;
+  }
+</script>
