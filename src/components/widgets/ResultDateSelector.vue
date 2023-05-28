01d8e93c4a4c8d7fb7784f11e4de678a7d6edd61
Locked15 <almgal353@gmail.com>
2023-05-28 22:04:58 +0500

    New widget to select target date (new API feature).

---
+++ src/components/widgets/ResultDateSelector.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <div class="result-selection-parent">
+      <div class="result-selection-row">
+        <span class="date-selection-panel" v-if="selectorModel.forTargetDatesSelectionMode">
+          <VueDatePicker multi-dates utc :flow="datePickerCalendarsFlow"
+                         locale="ru" cancelText="Отмена" selectText="Выбрать"
+                         :format="datePickerDateFormat" :preview-format="datePickerDateFormat"
+                         v-model="selectorModel.selectedTargetDates"
+                         @update:model-value="onTargetItemsSelectionChanged()"
+                         :teleport="true"
+                         :dark="isCurrentThemeDark"
+                         :clearable="false"
+                         placeholder="Укажите целевые даты"
+                         :class="selectorModel.selectedTargetDates.length > 0 ? 'date-selector-active' : ''" />
+
+          <v-btn v-if="selectorModel.selectedTargetDates.length > 0" @click="onClearTargetDatesSelectionClick()">
+            Очистить
+          </v-btn>
+        </span>
+        <span class="day-index-selection-parent" v-else>
+          <v-select label="Какие дни нужно отобразить?"
+                    :items="selectorModel.ALL_AVAILABLE_DAYS"
+                    v-model="selectorModel.selectedDays"
+                    @update:model-value="onTargetItemsSelectionChanged"
+                    item-title="name"
+                    item-value="name"
+                    return-object
+                    multiple
+                    chips />
+        </span>
+      </div>
+      <div class="result-type-selection">
+        <div class="type-switcher-parent">
+          <v-switch v-model="selectorModel.forTargetDatesSelectionMode"
+                    @update:model-value="onSelectionModeChanges()"
+                    :label="'Выбор по дате'" inset />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import VueDatePicker from '@vuepic/vue-datepicker';
+  import ResultMessages from '@/models/common/messages/ResultMessages';
+  import ResultDateSelectorModel from '@/models/components/widgets/ResultDateSelectorModel';
+  import Swal from 'sweetalert2';
+  import { Vue, Options } from 'vue-class-component';
+  import { getDarkStateOfCurrentTheme } from '@/common/utils/ColorThemeWorker';
+
+  @Options({
+    components: {
+      VueDatePicker,
+    },
+    props: {
+      useDBAsSource: Boolean,
+      selectorModel: ResultDateSelectorModel,
+    },
+  })
+
+  export default class ResultDateSelector extends Vue {
+    public useDBAsSource!: boolean;
+
+    public selectorModel!: ResultDateSelectorModel;
+
+    public isCurrentThemeDark = getDarkStateOfCurrentTheme();
+
+    public datePickerCalendarsFlow = ['year', 'month', 'calendar'];
+
+    public data = () => this.selectorModel;
+
+    // eslint-disable-next-line class-methods-use-this
+    public datePickerDateFormat(dates: Array<Date>): string {
+      if (dates != null) {
+        const formattedStrings = new Array<string>();
+        dates.forEach((date) => {
+          formattedStrings.push(`${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}!`);
+        });
+
+        return formattedStrings.join(', ');
+      }
+      return '';
+    }
+
+    public onSelectionModeChanges() {
+      if (!this.useDBAsSource && this.selectorModel.forTargetDatesSelectionMode) {
+        Swal.fire(
+          ResultMessages.SelectionByDateIsUnavailableOnNonDBSettings.title,
+          ResultMessages.SelectionByDateIsUnavailableOnNonDBSettings.message,
+          'warning',
+        );
+        this.selectorModel.forTargetDatesSelectionMode = false;
+      }
+    }
+
+    public onTargetItemsSelectionChanged() {
+      const selectedDaysCount = this.selectorModel.selectedDays.length;
+      const selectedDatesCount = this.selectorModel.selectedTargetDates.length;
+
+      if (selectedDaysCount < 1 || (selectedDatesCount < 1 && this.selectorModel.forTargetDatesSelectionMode)) {
+        Swal.fire(ResultMessages.ShowSelectionIsEmpty.title, ResultMessages.ShowSelectionIsEmpty.message, 'warning');
+      } else if (selectedDaysCount > 3 || (selectedDatesCount > 3 && this.selectorModel.forTargetDatesSelectionMode)) {
+        Swal.fire(ResultMessages.ShowSelectionIsLarge.title, ResultMessages.ShowSelectionIsLarge.message, 'warning');
+      }
+    }
+
+    public onClearTargetDatesSelectionClick() {
+      this.selectorModel.selectedTargetDates = new Array<Date>();
+      this.onTargetItemsSelectionChanged();
+    }
+  }
+</script>
+
+<style scoped>
+  .result-selection-parent {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .result-selection-parent > .result-selection-row {
+    align-self: center;
+
+    width: 50%;
+    max-width: 60%;
+    overflow: scroll;
+  }
+
+  .date-selection-panel {
+    display: flex;
+    flex: auto;
+    flex-direction: row;
+  }
+
+  .date-selection-panel > .date-selector-active {
+    max-width: 80%;
+    align-self: flex-start;
+  }
+
+  .result-type-selection {
+    display: flex;
+    flex: auto;
+    flex-direction: column;
+
+    margin-top: 12px;
+  }
+
+  .type-switcher-parent {
+    align-self: center;
+  }
+</style>
