6f3db216c3562e21f19cc8d7326378647ef5339d
Locked15 <almgal353@gmail.com>
2023-05-28 21:47:05 +0500

    Added API route models for new (V2) controllers and actions.
    This ones is marked as 'Modern'.

---
+++ src/models/api/routes/v2/other/ModernAPIGeneralRoutes.ts
@@ -0,0 +1,7 @@
+export default class ModernAPIGeneralRoutes {
+  public static readonly SEARCH_CONTROLLER = 'search';
+
+  public static readonly TEACHERS_SEARCH = 'teacher';
+
+  public static readonly SUMMARY_ACTION = 'all';
+}
---
+++ src/models/api/routes/v2/schedule/ModernAPIScheduleRoutes.ts
@@ -0,0 +1,21 @@
+export default class ModernAPIScheduleRoutes {
+  public static readonly BASIC_SCHEDULE_CONTROLLER = 'basic';
+
+  public static readonly SCHEDULE_REPLACEMENTS_CONTROLLER = 'replacement';
+
+  public static readonly FINAL_SCHEDULE_CONTROLLER = 'final';
+
+  public static readonly GROUP_SELECTOR = 'group';
+
+  public static readonly TEACHER_SELECTOR = 'teacher';
+
+  public static readonly DAY_SELECTOR = 'day';
+
+  public static readonly WEEK_SELECTOR = 'week';
+
+  public static readonly GROUP_DATE_TYPE_SELECTOR = 'date';
+
+  public static readonly TEACHER_DAY_INDEX_TYPE_SELECTOR = 'index-id';
+
+  public static readonly TEACHER_DATE_TYPE_SELECTOR = 'date-id';
+}
