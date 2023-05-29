edbf16623b265b1740ac38d326f0c3a2c8a5a200
Locked15 <almgal353@gmail.com>
2023-05-29 17:11:33 +0500

    Component is implemented.

--- src/components/common/result/ModernResultElement.vue
+++ src/components/common/result/ModernResultElement.vue
@@ -1,65 +1,98 @@
 <template>
   <div>
     <!-- Parent for Teacher-Based Schedules. -->
-    <div v-if="modelIsInstanceOfTeacherSchedule(model.data)">
+    <div v-if="modelIsInstanceOfTeacherSchedule()">
       <div class="result-item">
-        <p>Teacher Schedule!</p>
+        <ModernTeacherScheduleResultItem :itemModel="getItemModelForTeacherSchedule()" />
       </div>
     </div>
 
     <!-- Parent for Group-Based Schedules. -->
     <div v-else>
       <!-- Parent container for 'Basic Schedule' result. -->
-      <div class="result-item" v-if="modelIsInstanceOfBasicSchedule(model.data)">
-        <p>Group Basic Schedule!</p>
+      <div class="result-item" v-if="modelIsInstanceOfBasicSchedule()">
+        <ModernBasicScheduleResultItem :itemModel="getItemModelForBasicSchedule()" />
       </div>
 
       <!-- Parent container for 'Schedule Replacement' result. -->
-      <div class="result-item" v-else-if="modelIsInstanceOfScheduleReplacement(model.data)">
-        <p>Group Replacement Data!</p>
+      <div class="result-item" v-else-if="modelIsInstanceOfScheduleReplacement()">
+        <ModernScheduleReplacementResultItem :itemModel="getItemModelForScheduleReplacement()" />
       </div>
 
       <!-- Parent container for 'Final Schedule' result. -->
       <div class="result-item" v-else>
-        <p>Group Final Schedule!</p>
+        <ModernFinalScheduleResultItem :itemModel="getItemModelForFinalSchedule()" />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import ModernAPIEntitiesParent from '@/models/api/entities/v2/common/ModernAPIEntitiesParent';
   import {
     isInstanceOfGroupBasicScheduleWrapper,
     isInstanceOfGroupFinalScheduleWrapper,
     isInstanceOfGroupScheduleReplacementWrapper,
     isInstanceOfTeacherScheduleWrapper,
   } from '@/models/api/entities/v2/common/ModernEntityCastUtils';
+  import TeacherScheduleWrapper from '@/models/api/entities/v2/wrappers/TeacherScheduleWrapper';
+  import GroupBasicScheduleWrapper from '@/models/api/entities/v2/wrappers/group/GroupBasicScheduleWrapper';
+  import GroupFinalScheduleWrapper from '@/models/api/entities/v2/wrappers/group/GroupFinalScheduleWrapper';
+  import GroupScheduleReplacementWrapper from '@/models/api/entities/v2/wrappers/group/GroupScheduleReplacementWrapper';
   import ModernResultElementModel from '@/models/components/common/result/ModernResultElementModel';
   import { Options, Vue } from 'vue-class-component';
+  import ModernFinalScheduleResultItem from './types/v2/ModernFinalScheduleResultItem.vue';
+  import ModernBasicScheduleResultItem from './types/v2/ModernBasicScheduleResultItem.vue';
+  import ModernScheduleReplacementResultItem from './types/v2/ModernScheduleReplacementResultItem.vue';
+  import ModernTeacherScheduleResultItem from './types/v2/ModernTeacherScheduleResultItem.vue';
 
   @Options({
     props: {
       model: ModernResultElementModel,
     },
+    components: {
+      ModernBasicScheduleResultItem,
+      ModernScheduleReplacementResultItem,
+      ModernFinalScheduleResultItem,
+      ModernTeacherScheduleResultItem,
+    },
   })
   export default class ModernResultElement extends Vue {
     public model!: ModernResultElementModel;
 
-    // eslint-disable-next-line class-methods-use-this
-    public modelIsInstanceOfBasicSchedule = (obj: ModernAPIEntitiesParent) => isInstanceOfGroupBasicScheduleWrapper(obj);
+    public modelIsInstanceOfBasicSchedule(): boolean {
+      return isInstanceOfGroupBasicScheduleWrapper(this.model.data);
+    }
 
-    // eslint-disable-next-line class-methods-use-this, vue/max-len
-    public modelIsInstanceOfScheduleReplacement = (obj: ModernAPIEntitiesParent) => isInstanceOfGroupScheduleReplacementWrapper(obj);
+    public getItemModelForBasicSchedule(): GroupBasicScheduleWrapper {
+      return this.model.data as GroupBasicScheduleWrapper;
+    }
 
-    // eslint-disable-next-line class-methods-use-this
-    public modelIsInstanceOfFinalSchedule = (obj: ModernAPIEntitiesParent) => isInstanceOfGroupFinalScheduleWrapper(obj);
+    public modelIsInstanceOfScheduleReplacement(): boolean {
+      return isInstanceOfGroupScheduleReplacementWrapper(this.model.data);
+    }
 
-    // eslint-disable-next-line class-methods-use-this
-    public modelIsInstanceOfTeacherSchedule = (obj: ModernAPIEntitiesParent) => isInstanceOfTeacherScheduleWrapper(obj);
+    public getItemModelForScheduleReplacement(): GroupScheduleReplacementWrapper {
+      return this.model.data as GroupScheduleReplacementWrapper;
+    }
+
+    public modelIsInstanceOfFinalSchedule(): boolean {
+      return isInstanceOfGroupFinalScheduleWrapper(this.model.data);
+    }
+
+    public getItemModelForFinalSchedule(): GroupFinalScheduleWrapper {
+      return this.model.data as GroupFinalScheduleWrapper;
+    }
+
+    public modelIsInstanceOfTeacherSchedule(): boolean {
+      return isInstanceOfTeacherScheduleWrapper(this.model.data);
+    }
+
+    public getItemModelForTeacherSchedule(): TeacherScheduleWrapper {
+      return this.model.data as TeacherScheduleWrapper;
+    }
   }
 </script>
 
 <style>
-  @import '@/../public/css/result.css';
+  @import '@/../public/css/result/result-component.css';
 </style>
