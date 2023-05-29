41328cb59bf4d26166e31cb0d596f0044e726299
Locked15 <almgal353@gmail.com>
2023-05-29 13:22:10 +0500

    Rearrange of functions. Fixed mistake with incorrect return types. Teacher-Related types moved to Generic.

--- src/common/repository/base/IModernScheduleRepository.ts
+++ src/common/repository/base/IModernScheduleRepository.ts
@@ -2,12 +2,12 @@ import IScheduleAPIRepository from '@/common/repository/base/IScheduleAPIReposit
 import ModernAPIEntitiesParent from '@/models/api/entities/v2/common/ModernAPIEntitiesParent';
 
 // eslint-disable-next-line vue/max-len
-export default interface IModernScheduleRepository<T extends ModernAPIEntitiesParent> extends IScheduleAPIRepository {
+export default interface IModernScheduleRepository<T extends ModernAPIEntitiesParent, K extends ModernAPIEntitiesParent> extends IScheduleAPIRepository {
   getDataFromAPIForGroup(targetGroup: string, targetDate: Date, remainAttempts: number): Promise<T>;
-
-  getDataFromAPIForTeacher(teacherId: number, targetDate: Date, remainAttempts: number): Promise<T>;
 
   tryToGetDataFromAPIForGroup(targetGroup: string, targetDate: Date): Promise<T>;
 
-  tryToGetDataFromAPIForTeacher(teacherId: number, targetDate: Date): Promise<T>;
+  getDataFromAPIForTeacher(teacherId: number, targetDate: Date, remainAttempts: number): Promise<K>;
+
+  tryToGetDataFromAPIForTeacher(teacherId: number, targetDate: Date): Promise<K>;
 }
