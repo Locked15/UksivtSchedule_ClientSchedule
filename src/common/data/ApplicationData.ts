4f67a6f372e3faac24137b6285fbe797344f04e6
Locked15 <almgal353@gmail.com>
2023-05-30 00:07:01 +0500

    Created new constant, that defines widest Narrow screen width.

--- src/common/data/ApplicationData.ts
+++ src/common/data/ApplicationData.ts
@@ -1,6 +1,8 @@
 import Teacher from '@/models/api/entities/v2/base/Teacher';
 
 export default class ApplicationData {
+  public static readonly CLIENT_NARROW_MAX_WIDTH = 480;
+
   public static availableGroups = new Array<string>();
 
   public static availableTeachers = new Array<Teacher>();
