800111cb4a4c41bc82f41ce74ced27f6c1107558
Locked15 <almgal353@gmail.com>
2023-06-04 19:24:23 +0500

    Altered WebpackConfig, to use custom app title.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+/* eslint-disable no-param-reassign */
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      // Set custom title for application.
+      args[0].title = 'Client Schedule';
+      return args;
+    });
+  },
 });
