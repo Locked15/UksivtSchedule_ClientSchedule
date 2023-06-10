af29f1873c2afd4448c59d76be908e93b1da4f5c
Locked15 <almgal353@gmail.com>
2023-06-11 01:32:29 +0500

    Some adjustments (temporarily) for footer content.

--- src/components/base/FooterComponent.vue
+++ src/components/base/FooterComponent.vue
@@ -1,6 +1,13 @@
 <template>
+  <div class="footer-margin" />
   <div class="footer-content">
     <!-- Time Solution. -->
     <article>Footer body here.</article>
   </div>
 </template>
+
+<style scoped>
+  .footer-margin {
+    margin-top: 64px;
+  }
+</style>
