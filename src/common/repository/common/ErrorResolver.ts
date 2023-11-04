7c0c41982876688d8ae4e67a5371fad9bb291ef7
Locked15 <79941113+Locked15@users.noreply.github.com>
2023-11-04 21:33:46 +0500

    Fixed problem with multiple alerts showing.

--- src/common/repository/common/ErrorResolver.ts
+++ src/common/repository/common/ErrorResolver.ts
@@ -4,15 +4,24 @@ import APIMessages from '@/models/common/messages/APIMessages';
 export default class ErrorResolver {
   private error: any;
 
+  private static isMessageShowsNow = false;
+
   public constructor(error: any) {
     this.error = error;
   }
 
   public alertAboutError() {
+    if (ErrorResolver.isMessageShowsNow) return;
+
+    ErrorResolver.isMessageShowsNow = true;
     if (!this.error.response || !this.error.response.status) {
-      Swal.fire(APIMessages.APINotAvailable.title, APIMessages.APINotAvailable.message, 'error');
+      Swal.fire(APIMessages.APINotAvailable.title, APIMessages.APINotAvailable.message, 'error').then(() => {
+        ErrorResolver.isMessageShowsNow = false;
+      });
     } else {
-      Swal.fire('Что-то пошло не так', this.error.response.data, 'error');
+      Swal.fire('Что-то пошло не так', this.error.response.data, 'error').then(() => {
+        ErrorResolver.isMessageShowsNow = false;
+      });
     }
   }
 }
