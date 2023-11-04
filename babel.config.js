bd8085b1aa7e228f0bed0f03b6e8914e60d9fa90
Locked15 <79941113+Locked15@users.noreply.github.com>
2023-11-04 22:39:18 +0500

    Added new babel rules to config file.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
+  presets: [
+    '@vue/cli-plugin-babel/preset',
+    '@babel/preset-typescript',
+    '@babel/preset-env',
+  ],
+  plugins: [
+    '@babel/plugin-transform-class-static-block',
+  ],
 };
